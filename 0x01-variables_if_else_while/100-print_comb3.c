a8a475bc6d01c8724c4c67de9297a9e5b6b8a945
eyadhashim <hashimeyad@gmail.com>
2023-10-07 14:09:53 -0700

    0

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: 0 (success)
+ */
+int main(void)
+{
+	int a = 0, b;
+
+
+	while (a <= 9)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			if (a != b && a < b)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				if (a + b != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
