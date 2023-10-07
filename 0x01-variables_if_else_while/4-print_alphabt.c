20fa5521599f4d74c7c8f99beec90f53373b6cd1
eyadhashim <hashimeyad@gmail.com>
2023-10-07 13:58:59 -0700

    0

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: program print all most alphabt
+  *
+  * Return: 0 (success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'q' || c == 'e')
+			c++;
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
