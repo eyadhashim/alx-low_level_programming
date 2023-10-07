16273965ed30de2067b4a3bcfe5e6cb73d7b4098
eyadhashim <hashimeyad@gmail.com>
2023-10-07 12:34:39 -0700

    0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: use puts function to print text
+  *
+  * Return: Alawys 0 (Success)
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
