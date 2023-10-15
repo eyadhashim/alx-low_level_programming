67bf25daaad48ab31530e4ec41a7f86bf7440dc2
eyadhashim <hashimeyad@gmail.com>
2023-10-15 13:36:26 -0700

    0

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  print_alphabet_x10 - print 10 tim
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = "a"; j <= "z"; j++)
+			_putchar(j);
+
+		_putchar("\n");
+	}
+}
