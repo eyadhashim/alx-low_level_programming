d187474dd3229ef8b3d9f9a651577654ce28b87e
eyadhashim <hashimeyad@gmail.com>
2023-10-24 06:56:02 -0700

    0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,11 +3,12 @@
 
 #include <stdio.h>
 
-#include <unistd.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void)i;
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
 
 #endif
