98ba2096360fde5fc06062eeed246633feebe921
eyadhashim <hashimeyad@gmail.com>
2023-10-26 22:12:30 -0700

    0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,9 +3,10 @@
 
 #include <stdio.h>
 
-int _putchar(char c):
-void print_alphbet(void):
-void print_alphbet_x10(void):
-int _islower(int c):
+int _putchar(char c);
+void print_alphbet(void);
+void print_alphbet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
 
 #endif
