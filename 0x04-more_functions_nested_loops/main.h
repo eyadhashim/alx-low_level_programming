c19fc96dabe74fc6aa76299bdc928e4ca955735c
eyad hashim <hashimeyad648@gmail.com>
2023-10-29 00:59:07 -0700

    0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,23 +1,6 @@
-#include <stdio.h>
-#include "main.h"
+#ifndef main_h
+#define main_h
 
-/**
-* main - prints the largest of 3 integers
-* Return: 0
-*/
-
-int main(void)
-{
-        int a, b, c;
-        int largest;
-
-        a = 972;
-        b = -98;
-        c = 0;
-
-        largest = largest_number(a, b, c);
-
-        printf("%d is the largest number\n", largest);
-
-        return (0);
-}
+int _putchar(char c);
+int _isupper(int c);
+#endif
