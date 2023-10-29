bfa7b86c7b155b544eefd9bb7a56456c34093bf0
eyad hashim <hashimeyad648@gmail.com>
2023-10-29 01:37:15 -0700

    0

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,24 +1,22 @@
 #include "main.h"
-
 /**
-  * print_diagonal - draw a diagonal line
-  * @n: input vlue
+  * print_square - print a sqaure
+  * @size: input value
  */
 
-void print_diagonal(int n)
+void print_square(int size)
 {
 	int a, b;
 
-	if (n > 0)
+	if (size < 1)
+		_putchar('\n');
+	else
 	{
-		for (a = 0; a < n; a++)
+		for (a = 0; a < size; a++)
 		{
-			for (b = 0; b < a; b++)
-				_putchar(' ');
-			_putchar('\\');
+			for (b = 0; b < size; b++)
+				_putchar('#');
 			_putchar('\n');
 		}
 	}
-	else
-		_putchar('\n');
 }
