475ebcfeb9a537782d5b0c99143b0bdb9ad82895
eyad hashim <hashimeyad648@gmail.com>
2023-10-29 08:15:57 -0700

    0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
 int putchar(char c);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+
 #endif
