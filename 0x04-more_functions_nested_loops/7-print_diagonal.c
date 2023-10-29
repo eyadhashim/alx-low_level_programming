efcb976f3d2a95829b623e4f6b5f87cf25d9a294
eyad hashim <hashimeyad648@gmail.com>
2023-10-29 01:17:26 -0700

    0

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_diagonal - draw a diagonal line
+  * @n: input vlue
+ */
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
