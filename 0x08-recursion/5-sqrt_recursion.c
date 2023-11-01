9b6ae29ba2eb7cc8ede0bfee989ca39c3bf559d4
eyad hashim <hashimeyad648@gmail.com>
2023-11-01 12:58:09 -0700

    0

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  * squar - natural square
+  * @n: input value
+  * @x: input value
+  * Return: the natual square
+ */
+
+int squar(int n, int x)
+{
+	if (x * x == n)
+		return (x);
+	else if (x * x < n)
+		return (squar(n, x + 1));
+	else
+		return (-1);
+
+}
+/**
+  * _sqrt_recursion - natural square
+  * @n: input
+  * Return: the value
+ */
+int _sqrt_recursion(int n)
+{
+	return (squar(n, 1));
+}
