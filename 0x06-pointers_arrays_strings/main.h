b4c08fbdbb10600587c9956080db4be57d027505
eyad hashim <hashimeyad648@gmail.com>
2023-11-04 23:30:31 -0700

    0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,5 +4,36 @@
 int putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+int is_low(char c);
+
+int is_del(char c);
+
+char *cap_string(char *);
+
+char *leet(char *str);
+
+char *rot13(char *str);
+
+void print_number(int n);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
+
+int isPrint(int n);
+
+void printHexes(char *b, int start, int end);
+
+
+void printASCII(char *b, int start, int end);
+
+#endif
 #endif /* MAIN_H */
